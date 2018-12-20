bf20d1ab1bd104e3f6259fa64c2cd2d8088a22d2
himanshu <eventmanageo@gmail.com>
2018-12-20 14:42:56 +0530

    Different blade for registration of admin and vendor is make.

--- eventomanagefull/resources/views/auth/login.blade.php
+++ eventomanagefull/resources/views/auth/login.blade.php
@@ -15,7 +15,6 @@
                     @endisset
                         
                     @csrf
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
