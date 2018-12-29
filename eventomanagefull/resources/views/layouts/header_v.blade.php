256b467ded6b18ef625b41731ed1422e2b763fc7
himanshu <eventmanageo@gmail.com>
2018-12-29 16:46:01 +0530

    little conflicts solved

--- eventomanagefull/resources/views/layouts/header_v.blade.php
+++ eventomanagefull/resources/views/layouts/header_v.blade.php
@@ -11,32 +11,21 @@
   <title>{{ config('app.name', 'Laravel') }}</title>
 
   <!-- Scripts -->
-  <script src="{{ asset('js/app.js') }}"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-
-  <!-- jQuery CDN - Slim version (=without AJAX) -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
   <!-- Popper.JS -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-
   <!-- Bootstrap JS -->
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-  
   <!-- jQuery Custom Scroller CDN -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
-
   <!-- Font Awesome JS -->
   <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"></script>
   <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"></script>
-
   <!-- Fonts -->
   <link rel="dns-prefetch" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-  
   <!-- Styles -->
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/style.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href="{{ asset('css/style_a.css') }}" rel="stylesheet">
@@ -84,7 +73,7 @@
               <a href="/company/details/{{Session::get('vendor_category')}}">Add Company Details</a>
           </li>
           <li>
-            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Service Management</a>
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Service Management</a>
             <ul class="collapse list-unstyled" id="homeSubmenu">
               <li>
                   <a href="/{{Session::get('vendor_category')}}/add/service">Add services</a>
@@ -105,8 +94,8 @@
             </ul>
           </li>
           <li>
-            <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                      document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+              <a href="{{ route('logout') }}" onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
           </li>
         </ul>
       </nav>
@@ -127,6 +116,9 @@
                 </li>
               </ul>
             </div> --}}
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
           </div>
         </nav>
       </div>
--- eventomanagefull/resources/views/vendor/services/catererservice.blade.php
+++ eventomanagefull/resources/views/vendor/services/catererservice.blade.php
@@ -20,87 +20,83 @@
                 alert('Failed');
             </script>
         @endif
-
-        <form action="" method="POST" class="form" enctype="multipart/form-data">
+        
+        <form action="" method="POST" id="caterer-form" class="form" enctype="multipart/form-data">
             @csrf
-            <div class="card mt-2">
-                <div class="card-body">
-                    <div class="row form-group">
-                        <div class="col-md-3 text-left col-form-label">
-                            <label>Item Name</label>
-                        </div>
-                        <div class="col-md-6">
-                            <input type="text" class="form-control" id="item_name" name="item_name" required/>
-                        </div>
+            <div class="row form-group">
+                <div class="col-md-3 text-left col-form-label">
+                    <label>Item Name</label>
+                </div>
+                <div class="col-md-6">
+                    <input type="text" class="form-control" id="item_name" name="item_name" required/>
+                </div>
+            </div>
+            <div class="row form-group">
+                <div class="col-md-3 text-left col-form-label">
+                    <label>Item Dine Time</label>
+                </div>
+                <div class="col-md-6 text-left">
+                    <label class="radio radio-control">
+                        <input type="radio" name="item_dine_time" value="breakfast" checked required> Breakfast
+                    </label>
+                    &nbsp;
+                    <label class="radio radio-control">
+                        <input type="radio" name="item_dine_time" value="lunch" required> Lunch
+                    </label>
+                    &nbsp;
+                    <label class="radio radio-control">
+                        <input type="radio" name="item_dine_time" value="dinner" required> Dinner
+                    </label>
+                </div>
+            </div>
+            <div class="row form-group">
+                    <div class="col-md-3 text-left col-form-label">
+                        <label>Item Category</label>
                     </div>
-                    <div class="row form-group">
-                        <div class="col-md-3 text-left col-form-label">
-                            <label>Item Dine Time</label>
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <label class="radio radio-control">
-                                <input type="radio" name="item_dine_time" value="breakfast" checked required> Breakfast
-                            </label>
-                            &nbsp;
-                            <label class="radio radio-control">
-                                <input type="radio" name="item_dine_time" value="lunch" required> Lunch
-                            </label>
-                            &nbsp;
-                            <label class="radio radio-control">
-                                <input type="radio" name="item_dine_time" value="dinner" required> Dinner
-                            </label>
-                        </div>
+                    <div class="col-md-6 text-left">
+                        <label class="radio radio-control">
+                            <input type="radio" name="item_category" value="started" checked required> Starter
+                        </label>
+                        &nbsp;
+                        <label class="radio radio-control">
+                            <input type="radio" name="item_category" value="maincourse" required> Main Course
+                        </label>
+                        &nbsp;
+                        <label class="radio radio-control">
+                            <input type="radio" name="item_category" value="desserts" required> Desserts
+                        </label>
                     </div>
-                    <div class="row form-group">
-                            <div class="col-md-3 text-left col-form-label">
-                                <label>Item Category</label>
-                            </div>
-                            <div class="col-md-6 text-left">
-                                <label class="radio radio-control">
-                                    <input type="radio" name="item_category" value="started" checked required> Starter
-                                </label>
-                                &nbsp;
-                                <label class="radio radio-control">
-                                    <input type="radio" name="item_category" value="maincourse" required> Main Course
-                                </label>
-                                &nbsp;
-                                <label class="radio radio-control">
-                                    <input type="radio" name="item_category" value="desserts" required> Desserts
-                                </label>
-                            </div>
-                        </div>
-                    <div class="row form-group">
-                        <div class="col-md-3 text-left col-form-label">
-                            <label>Item Description</label>
-                        </div>
-                        <div class="col-md-6">
-                            <textarea class="form-control" cols="5" rows="5" id="item_description" name="item_description" required></textarea>
-                        </div>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-md-3 text-left col-form-label">
-                            <label>Item Picture</label>
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <input type="file" accept="image/jpg" id="item_picture" name="item_picture" required/>
-                        </div>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-md-3 text-left col-form-label">
-                            <label>Item Price</label>
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <input class="form-control" type="text" id="item_price" name="item_price"/>
-                        </div>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-md-1 offset-3">
-                            <input class="btn btn-success" type="submit" id="submit" value="Submit" name="submit"/>
-                        </div>
-                        <div class="col-md-1 ml-5">
-                            <input class="btn" type="reset" id="submit" value="Reset" name="submit"/>
-                        </div>
-                    </div>
+                </div>
+            <div class="row form-group">
+                <div class="col-md-3 text-left col-form-label">
+                    <label>Item Description</label>
+                </div>
+                <div class="col-md-6">
+                    <textarea class="form-control" cols="5" rows="5" id="item_description" name="item_description" required></textarea>
+                </div>
+            </div>
+            <div class="row form-group">
+                <div class="col-md-3 text-left col-form-label">
+                    <label>Item Picture</label>
+                </div>
+                <div class="col-md-6 text-left">
+                    <input type="file" accept="image/jpg" id="item_picture" name="item_picture" required/>
+                </div>
+            </div>
+            <div class="row form-group">
+                <div class="col-md-3 text-left col-form-label">
+                    <label>Item Price</label>
+                </div>
+                <div class="col-md-6 text-left">
+                    <input class="form-control" type="text" id="item_price" name="item_price"/>
+                </div>
+            </div>
+            <div class="row form-group">
+                <div class="col-md-1 offset-3">
+                    <input class="btn btn-success" type="submit" id="submit" value="Submit" name="submit" />
+                </div>
+                <div class="col-md-1 ml-5">
+                    <input class="btn" type="reset" id="reset" value="Reset" name="submit"/>
                 </div>
             </div>
         </form>
